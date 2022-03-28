00e83c7be67d4fef30133bd6b6ba311c7259b8f8
Marcos Sousa <a.marcos.car@bol.com.br>
2022-03-28 20:54:34 -0300

    Novo desafio de loops com for completo

--- scripts.py
+++ scripts.py
@@ -1,3 +1,5 @@
+# Exercicios com if, elif, else. 
+
 """
 Task
 Given an integer, , perform the following conditional actions:
@@ -7,18 +9,30 @@ If n is even and in the inclusive range of 2 to 5, print Not Weird
 If n is even and in the inclusive range of 6 to 20, print Weird
 If n is even and greater than 20, print Not Weird"""
 
-n = 0
+n = int(input(': '))
 
-
-if n % 2 == 0 and n < 20:
+if n % 2 == 0 and n < 2:
     print('Weird')
 
-elif n % 2 == 1 and n >= 6 <= 20:
-    print('Weird')
-
-else:
+elif n % 2 == 1 and n in range(2,  6) or n > 20:
     print('Not Weird')
 
-# Codigo não funciona em alguns testes
+else:
+    print('Weird')
+# Codigo não funciona em alguns testes no HackerRanck
 
 
+# Exercicios com loops
+"""
+Task
+The provided code stub reads and integer, , from STDIN. For all 
+non-negative integers i < n, print i².
+"""
+
+n = int(input(': '))
+
+for x in range(n):
+    n += 1
+    x **= 2
+    print(x)
+# Codigo funciona normalmente. Estudar possivel refatoração
