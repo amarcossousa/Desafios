407277f32622b3564ffb0edae759f061cce5a5e1
Marcos Sousa <a.marcos.car@bol.com.br>
2022-03-28 20:03:02 -0300

    Primeira versão, não funciona em 3/8 testes, codigos comentados apagados

--- scripts.py
+++ scripts.py
@@ -7,37 +7,18 @@ If n is even and in the inclusive range of 2 to 5, print Not Weird
 If n is even and in the inclusive range of 6 to 20, print Weird
 If n is even and greater than 20, print Not Weird"""
 
-# n = int(input('Entre com um valor inteiro: '))
+n = 0
 
-# if n % 2 == 0 and n <= 2:
-#     print('Weird')
-# else: 
-#     print('Not Weird')
 
-# if n % 2 == 1 and n > 2 <= 5:
-#     print('Not Weird')
-# else:
-#     print('Weird')
+if n % 2 == 0 and n < 20:
+    print('Weird')
 
-# elif n % 2 == 0 and n > 2 <= 5:
-#     print('Not Weird')
+elif n % 2 == 1 and n >= 6 <= 20:
+    print('Weird')
 
-# elif n % 2 == 0 and n > 6 <= 20:
-#     print('Weird')
+else:
+    print('Not Weird')
 
-# if n % 2 == 1 and n > 24:
-#     print('Not Weird')
+# Codigo não funciona em alguns testes
 
-# else:
-#     print('Not Weird')
 
-n = int(input())
-cont = 0
-while cont < n:
-    n = n + 1
-    print(n)
-
-# n = int(input("Numero"))
-# cont = 0
-# while n >=cont:
-#     print(n)
