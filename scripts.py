7abb8058f946f4813f6a5c8b76a13a56a56f9900
Marcos Sousa <a.marcos.car@bol.com.br>
2022-03-29 20:55:22 -0300

    Comentario na refatoração do codigo com loop for

--- scripts.py
+++ scripts.py
@@ -36,8 +36,10 @@ n = int(input(': '))
 
 for i in range(n):
     print(i**2)
-    i **= 2
-    print(i)
+    """
+    # i **= 2
+    # print(i) # Parte refatorada do codigo
+    """ 
 # Codigo final
 
 # Exercicio com int e strings
