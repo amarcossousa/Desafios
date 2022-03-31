9c7195ef29cb38f05a14ea992b87aae00379e153
Marcos Sousa <a.marcos.car@bol.com.br>
2022-03-31 20:10:01 -0300

    Inserido função que checa se o ano é bissexto ou não

--- scripts.py
+++ scripts.py
@@ -36,10 +36,10 @@ n = int(input(': '))
 
 for i in range(n):
     print(i**2)
-    """
-    # i **= 2
-    # print(i) # Parte refatorada do codigo
-    """ 
+"""
+# i **= 2
+# print(i) # Parte refatorada do codigo
+""" 
 # Codigo final
 
 # Exercicio com int e strings
@@ -54,7 +54,40 @@ n = int(input(': '))
 
 for i in range (1, n+1):
     print (i, end="") # Usa a função 'end=""' para organizar o valor em linha
-# # Codigo completo
+# # # Codigo completo
+
+
+    
+""""
+An extra day is added to the calendar almost every four years as February 29, 
+and the day is called a leap day. It corrects the calendar for the fact that our 
+planet takes approximately 365.25 days to orbit the sun. A leap year contains a leap day.
+
+In the Gregorian calendar, three conditions are used to identify leap years:
+
+The year can be evenly divided by 4, is a leap year, unless:
+The year can be evenly divided by 100, it is NOT a leap year, unless:
+The year is also evenly divisible by 400. Then it is a leap year.
+This means that in the Gregorian calendar, the years 2000 and 2400 are leap years,
+ while 1800, 1900, 2100, 2200, 2300 and 2500 are NOT leap years.                             
+"""
+# Exercio com função e condicionais 
+# Codigo passa nos teste, mas precisa de refatoração
+
+def is_leap(year):
+    leap = False
+    if year % 400 == 0 or year % 4 == 0 and year < 2000:
+        leap = leap
+        return True
+    elif year % 100 == 0:
+        leap = leap
+        return False
+
+    return leap
+
+
+
+
 
 
 
