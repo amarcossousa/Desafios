f2c5efdfd423d0f778445a054b7a7f4c161b270b
Marcos Sousa <a.marcos.car@bol.com.br>
2022-04-06 16:02:59 -0300

    adicionado função do unittest

--- teste.py
+++ teste.py
@@ -1,5 +1,18 @@
+from unittest import TestCase, main
+# from scripts import *
+
+class Teste(TestCase):
+    def teste_função_de_compressão_de_lista(self):
+        pass
+
 x = int(input(': '))
 y = int(input(': '))
 z = int(input(': '))
 n = int(input(': '))
 lista = x, y, z
+print(lista )
+
+
+if __name__ == '__main__':
+    main()
+
