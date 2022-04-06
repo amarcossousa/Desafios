4699951819ce27f398d28b04d5eb5b7369c095f8
Marcos Sousa <a.marcos.car@bol.com.br>
2022-04-06 12:36:13 -0300

    Inicio da solução de compressão de lista

--- scripts.py
+++ scripts.py
@@ -9,19 +9,19 @@ If n is even and in the inclusive range of 2 to 5, print Not Weird
 If n is even and in the inclusive range of 6 to 20, print Weird
 If n is even and greater than 20, print Not Weird"""
 
-n = int(input(': '))
+# n = int(input(': '))
 
-if n % 2 == 1:
-    print('Weird')
+# if n % 2 == 1:
+#     print('Weird')
 
-elif n % 2 == 0 and n in range(2, 6):
-    print('Not Weird')
+# elif n % 2 == 0 and n in range(2, 6):
+#     print('Not Weird')
 
-elif n % 2 == 0 and n in range(6, 21):
-    print('Weird')
+# elif n % 2 == 0 and n in range(6, 21):
+#     print('Weird')
 
-else:
-    print('Not Weird')
+# else:
+#     print('Not Weird')
 
 # Codigo com pequena correção. Necessario possivel refatoração
 
@@ -32,11 +32,11 @@ Task
 The provided code stub reads and integer, , from STDIN. For all 
 non-negative integers i < n, print i²."""
 
-n = int(input(': '))
+# n = int(input(': '))
 
-for i in range(n):
-    print(i**2)
-"""
+# for i in range(n):
+#     print(i**2)
+# """
 # i **= 2
 # print(i) # Parte refatorada do codigo
 """ 
@@ -44,17 +44,17 @@ for i in range(n):
 
 # Exercicio com int e strings
 """
-The included code stub will read an integer, n, from STDIN.
-Without using any string methods, try to print the following:
-123...n
-Note that "..." represents the consecutive values in between
-"""
+# The included code stub will read an integer, n, from STDIN.
+# Without using any string methods, try to print the following:
+# 123...n
+# Note that "..." represents the consecutive values in between
+# """
 
-n = int(input(': '))
+# n = int(input(': '))
 
-for i in range (1, n+1):
-    print (i, end="") # Usa a função 'end=""' para organizar o valor em linha
-# # # Codigo completo
+# for i in range (1, n+1):
+#     print (i, end="") # Usa a função 'end=""' para organizar o valor em linha
+# # # # Codigo completo
 
 
     
@@ -74,17 +74,30 @@ This means that in the Gregorian calendar, the years 2000 and 2400 are leap year
 # Exercio com função e condicionais 
 # Codigo passa nos teste, mas precisa de refatoração
 
-def is_leap(year):
-    leap = False
-    if year % 400 == 0 or year % 4 == 0 and year < 2000:
-        leap = leap
-        return True
-    elif year % 100 == 0:
-        leap = leap
-        return False
-    return leap
+# def is_leap(year):
+#     leap = False
+#     if year % 400 == 0 or year % 4 == 0 and year < 2000:
+#         leap = leap
+#         return True
+#     elif year % 100 == 0:
+#         leap = leap
+#         return False
+#     return leap
 # Codigo completo sem refatoração 
 
+
+# Commpressão de listas
+x = int(input(': '))
+y = int(input(': '))
+z = int(input(': '))
+n = int(input(': '))
+
+lista = []
+n = 0
+cond = [x, y, z]
+for cont in range(list(cond)):
+    lista.append(cont)
+    print(lista)
 
 
 
